ca0f61eee2ff8fcf1fd3a58c4403ba3d7facefbc
Данил <danil@MacBook-Air-tilhrende-Danil.local>
2026-05-24 20:38:16 +0300

    add token

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Astro Finance Bot started")
+pimport asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+TOKEN = "8788138114:AAHnpHY8zzZy_ENvSWb9KwjchtVyJfH_7QM"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Бот запущен 🚀")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
