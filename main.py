8e66ff450f0995491755ae2f1a0f09f61f764e92
Данил <danil@MacBook-Air-tilhrende-Danil.local>
2026-05-25 10:18:31 +0300

    add profile handler

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-
+from handlers import profile
 from handlers import start, menu
 
 TOKEN = "8788138114:AAHnpHY8zzZy_ENvSWb9KwjchtVyJfH_7QM"
@@ -11,7 +11,7 @@ dp = Dispatcher()
 # ✅ подключаем роутеры
 dp.include_router(start.router)
 dp.include_router(menu.router)
-
+dp.include_router(profile.router)
 
 async def main():
     await dp.start_polling(bot)
