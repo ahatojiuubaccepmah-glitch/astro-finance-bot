c96c29d493f3ea1847706940947b0e67116a50d5
Данил <danil@MacBook-Air-tilhrende-Danil.local>
2026-05-25 16:58:09 +0300

    add AstrologyCalFull

--- keyboards/finance_menu.py
+++ keyboards/finance_menu.py
@@ -4,9 +4,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def get_finance_menu():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📊 Прогноз")],
-            [KeyboardButton(text="💰 Удачные дни")],
+            [KeyboardButton(text="📅 Календарь")],
             [KeyboardButton(text="⬅️ Назад")]
         ],
         resize_keyboard=True
     )
+``
